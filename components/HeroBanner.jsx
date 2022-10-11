63e3e3a140380f60aa83cf355598bf546e39c1b8
fansters <edmundseizentals@gmail.com>
2022-10-11 14:48:33 +0300

    connected sanity to BE. created components and exported them. created lib for sanity client and connected it. created products in sanity and puttimng dynamic data in code from sanity.

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+import { urlFor } from "../lib/client";
+const HeroBanner = ({ HeroBanner }) => {
+	return (
+		<div className='hero-banner-container'>
+			<div>
+				<p className='cream-solo'> {HeroBanner.smallText}</p>
+				<h3>{HeroBanner.midText}</h3>
+				<h1>{HeroBanner.largeText1}</h1>
+				<img src={urlFor(HeroBanner.image)} alt='kremi' className='hero-banner-image' />
+
+				<div className=''>
+					<Link href={`/product/${HeroBanner.product}`}>
+						<button type='buton'>{HeroBanner.buttonText}</button>
+					</Link>
+					<div className='desc'>
+						<h5>Description</h5>
+						<p> {HeroBanner.desc}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HeroBanner;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
-export default Home
+const Home = ({ products, bannerData }) => {
+	return (
+		<>
+         <HeroBanner HeroBanner={ bannerData.length && bannerData[0]} />
+			<div className='products-heading'>
+				<h2>Jaunākie produki</h2>
+				<p>Krēmi sejas ādai un mitrināšanai.</p>
+			</div>
+			<div className='products-container'>{products?.map((pro) => pro.name )}</div>
+
+			<FooterBanner />
+		</>
+	);
+};
+
+export const getServerSideProps = async () => {
+	const query = '*[_type == "product"]';
+	const products = await client.fetch(query);
+	const bannerQuery = '*[_type == "banner"]';
+	const bannerData = await client.fetch(bannerQuery);
+
+	return {
+		props: { products, bannerData },
+	};
+};
+
+export default Home;

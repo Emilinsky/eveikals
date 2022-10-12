5215f9f49842f94307dfec774a093705b04f7b0b
fansters <edmundseizentals@gmail.com>
2022-10-12 12:53:28 +0300

    completed with design of individual product. Added animations and hoverinig images. Continueing with product page logic

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,22 +1,24 @@
 import React from "react";
 import { client, urlFor } from "../../lib/client";
-
+import { useState } from 'react';
+import { Product } from "../../components";
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai";
 
 const ProductDetails = ({ product, products }) => {
 	const { image, name, details, price } = product;
+   const [index, setIndex] = useState(0)
 	return (
 		<div>
 			<div className='product-detail-container'>
 				<div>
 					<div className='image-container'>
-						<img src={urlFor(image && image[0])} />
+						<img src={urlFor(image && image[index])} className='product-detail-image' />
 					</div>
-					{/* <div className='small-images-container'>
+					<div className='small-images-container'>
                   {image?.map((item, i) => (
-                     <img src={urlFor(item)} className='' onMouseEnter='' />
+                     <img src={urlFor(item)} className={i ===index? 'small-image selected-image' : 'small-image'} onMouseEnter={()=> setIndex(i)} />
                   ))}
-               </div> */}
+               </div>
 				</div>
 				<div className='product-detail-desc'>
 					<h1>{name}</h1>
@@ -47,10 +49,24 @@ const ProductDetails = ({ product, products }) => {
 							</span>
 						</p>
 					</div>
-               <div className='buttons'>
-                  <button type='button' className='add-to-cart' onClick=''>Pievienot grozam</button>
-                  <button type='button' className='buy-now' onClick=''>Nopirkt uzreiz</button>
-               </div>
+					<div className='buttons'>
+						<button type='button' className='add-to-cart' onClick=''>
+							Pievienot grozam
+						</button>
+						<button type='button' className='buy-now' onClick=''>
+							Nopirkt uzreiz
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className='similar-products-wrapper'>
+				<h2>Lidzigie produkti:</h2>
+				<div className='marquee'>
+					<div className='similar-products-container track'>
+						{products.map((item) => (
+							<Product key={item._id} product={item} />
+						))}
+					</div>
 				</div>
 			</div>
 		</div>

6ef30d84df674d8865d504b5354a1aa8d6d1352a
fansters <edmundseizentals@gmail.com>
2022-10-14 15:35:10 +0300

    working with stripe imp. going through docs. Creating custom shipping tax and connecting stripe.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -2,13 +2,30 @@ import React, { useRef } from "react";
 import Link from "next/link";
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from "react-icons/ai";
 import { TiDeleteOutline } from "react-icons/ti";
-import { Toast } from "react-hot-toast";
+import toast from "react-hot-toast";
+
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
+import getStripe from "../lib/getStripe";
 
 const Cart = () => {
 	const cartRef = useRef();
-	const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity , onRemove} = useStateContext();
+	const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
+
+	const handleCheckout = async () => {
+		const stripe = await getStripe();
+		const response = await fetch("/api/stripe", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(cartItems),
+      });
+      
+		if (response.statusCode === 500) return;
+		const data = await response.json();
+		toast.loading("Uz maksājumu...");
+		stripe.redirectToCheckout({ sessionId: data.id });
+	};
+
 	return (
 		<div className='cart-wrapper' ref={cartRef}>
 			<div className='cart-container'>
@@ -53,7 +70,7 @@ const Cart = () => {
 												</span>
 											</p>
 										</div>
-										<button type='button' className='remove-item' onClick={()=>onRemove(item)}>
+										<button type='button' className='remove-item' onClick={() => onRemove(item)}>
 											<TiDeleteOutline />
 										</button>
 									</div>
@@ -68,7 +85,7 @@ const Cart = () => {
 							<h3>${totalPrice}</h3>
 						</div>
 						<div className='btn-container'>
-							<button type='button' className='btn' onClick=''>
+							<button type='button' className='btn' onClick={handleCheckout}>
 								Pirkt
 							</button>
 						</div>

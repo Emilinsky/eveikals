ab02abf036f3c73203c5008d0ee48f02226ebf0a
fansters <edmundseizentals@gmail.com>
2022-10-15 21:13:04 +0300

    implementing stripe. fixing errors. obj not showing in terminal(undef). debugging code

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,6 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
 	if (req.method === "POST") {
+		console.log(req.body.cartItems);
 		try {
 			const params = {
 				submit_type: "pay",
@@ -16,20 +17,34 @@ export default async function handler(req, res) {
 						shipping_rate: "shr_1LsmSKGSMAjLVvVP2mZpng5c",
 					},
 				],
-				line_items: [
-					{
-						// Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-						price: "{{PRICE_ID}}",
-						quantity: 1,
-					},
-				],
-				mode: "payment",
+				line_items: req.body.cartItems.map((item) => {
+					const img = item.image[0].asset._ref;
+					const newImage = img
+						.replace("image-", "https://cdn.sanity.io/images/1kdn8ha8/production/")
+						.replace("-webp", ".webp", "-jpeg", ".jpeg", "-png", ".png");
+					// console.log("IMAGE", newImage);
+					return {
+						price: {
+							currency: "eur",
+							producy_data: {
+								name: item.name,
+								images: [newImage],
+							},
+							unit_amount: item.price * 100,
+						},
+						ajustable_quantity: {
+							enabled: true,
+							minimum: 1,
+						},
+						quantity: item.quantity,
+					};
+				}),
 				success_url: `${req.headers.origin}/?success=true`,
 				cancel_url: `${req.headers.origin}/?canceled=true`,
 			};
 			// Create Checkout Sessions from body params.
 			const session = await stripe.checkout.sessions.create(params);
-			res.redirect(303, session.url);
+			res.status(200).json(session);
 		} catch (err) {
 			res.status(err.statusCode || 500).json(err.message);
 		}

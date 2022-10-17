d320f23d44961e6bf4d2920cca51b296802d5ccd
fansters <edmundseizentals@gmail.com>
2022-10-17 15:21:19 +0300

    error fixed. wrong shipping tax id. diffrerent curriencies in payments and shipping tax.

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,27 +4,19 @@ const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
 	if (req.method === "POST") {
-		console.log(req.body.cartItems);
 		try {
 			const params = {
 				submit_type: "pay",
 				mode: "payment",
 				payment_method_types: ["card"],
-				billing_adress_collection: "auto",
-				shipping_options: [
-					{
-						shipping_rate: "shr_1LsmMjGSMAjLVvVPsEw5DPB2",
-					},
-					{
-						shipping_rate: "shr_1LsmSKGSMAjLVvVP2mZpng5c",
-					},
-				],
+				billing_address_collection: "auto",
+				shipping_options: [{ shipping_rate: "shr_1LsmMjGSMAjLVvVPsEw5DPB2" }],
 				line_items: req.body.map((item) => {
 					const img = item.image[0].asset._ref;
 					const newImage = img
-						.replace("image-", "https://cdn.sanity.io/images/1kdn8ha8/production/")
-						.replace("-webp", ".webp", "-jpeg", ".jpeg", "-png", ".png");
-					// console.log("IMAGE", newImage);
+						.replace("image-", "https://cdn.sanity.io/images/vfxfwnaw/production/")
+						.replace("-webp", ".webp");
+
 					return {
 						price_data: {
 							currency: "eur",
@@ -41,12 +33,13 @@ export default async function handler(req, res) {
 						quantity: item.quantity,
 					};
 				}),
-				success_url: `${req.headers.origin}/?success=true`,
-				cancel_url: `${req.headers.origin}/?canceled=true`,
-         };
-         // new comm
+				success_url: `${req.headers.origin}/success`,
+				cancel_url: `${req.headers.origin}/canceled`,
+			};
+
 			// Create Checkout Sessions from body params.
 			const session = await stripe.checkout.sessions.create(params);
+
 			res.status(200).json(session);
 		} catch (err) {
 			res.status(err.statusCode || 500).json(err.message);

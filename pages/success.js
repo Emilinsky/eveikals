33dce0c6359ad6ea2c871bb8ef8d97d1ca9c8777
fansters <edmundseizentals@gmail.com>
2022-10-17 15:42:03 +0300

    everthing works now with stripe. redirect happens, purchase is captured. Showing error msg for succes page. Createing success page. Need to confirm stripe profile for real purchases

---
+++ pages/success.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-iconsbs";
+import { useRouter } from "next/router";
+
+import { useStateContext } from "../context/StateContext";
+
+const Success = () => {
+	return <div>Success</div>;
+};
+
+export default Success;

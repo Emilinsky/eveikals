dae9d742b4764ed55acbcb03420a1f95b396064c
fansters <edmundseizentals@gmail.com>
2022-10-17 16:23:44 +0300

    implemented confetti, erro is gone. New error when clicking on shop now banner. Product not found.

--- pages/success.js
+++ pages/success.js
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { BsBagCheckFill } from "react-iconsbs";
-import { useRouter } from "next/router";
+import { BsBagCheckFill } from "react-icons/bs";
+import { runFireworks } from "../lib/utils";
 
 import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
-	return <div>Success</div>;
+	const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+	useEffect(() => {
+		localStorage.clear();
+		setCartItems([]);
+		setTotalPrice(0);
+		setTotalQuantities(0);
+		runFireworks();
+	}, []);
+
+	return (
+		<div className='success-wrapper'>
+			<div className='success'>
+				<p className='icon'>
+					<BsBagCheckFill />
+				</p>
+				<h2 className=''>Paldies par pirkumu!</h2>
+				<p className='email-msg'>Uz epastu nosutijam rekvizitus!</p>
+				<p className='description'>
+					Jautajumu gadijuma sutie mums zinu uz:
+					<a className='email' href='mailto:edmundseizentals@gmail.com'>
+						edmundseizentals@gmail
+					</a>
+				</p>
+				<Link href='/'>
+					<button type='button' width='300px' className='btn'>
+						Apskatit citus produktus
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Success;

920936ee8d1ce6a112744516e2b5db51d3ef2616
Emilinsky <emilinsky260@gmail.com>
2023-05-21 22:14:30 +0100

    Added event listener console log when pressing mobile menu button, button works in Chromium, but doesnt work in Opera GX, still needs work to make the menu be in front of body of page, Navbar.jsx is pretty much a new version modified by GPT, fixed a bunch of undesireable code for navbar in CSS, Navbar looks better now

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,63 +6,86 @@ import { Cart } from "./";
 import { useStateContext } from "../context/StateContext";
 
 const Navbar = () => {
-	const { showCart, setShowCart, totalQuantities } = useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
-	const primaryNavRef = useRef(null);
-	const navToggleRef = useRef(null);
+  const primaryNavRef = useRef(null);
+  const navToggleRef = useRef(null);
 
-	useEffect(() => {
-		const primaryNav = primaryNavRef.current;
-		const navToggle = navToggleRef.current;
+  useEffect(() => {
+    const primaryNav = primaryNavRef.current;
+    const navToggle = navToggleRef.current;
 
-		navToggle.addEventListener("click", () => {
-			const visibility = primaryNav.getAttribute("data-visible");
+    const handleClick = () => {
+      const visibility = primaryNav.getAttribute("data-visible");
 
-			if (visibility === "false") {
-				primaryNav.setAttribute("data-visible", "true");
-				navToggle.setAttribute("aria-expanded", "true");
-			} else if (visibility === "true") {
-				primaryNav.setAttribute("data-visible", "false");
-				navToggle.setAttribute("aria-expanded", "false");
-			}
-		});
-	}, []);
+      if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", "true");
+        navToggle.setAttribute("aria-expanded", "true");
+      } else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", "false");
+        navToggle.setAttribute("aria-expanded", "false");
+      }
 
-	return (
-		<div className="navbar-container">
-			<div className="navbar-inner-cont">
-				<p className="logo">
-					<Link href="/">Shopfeast</Link>
-				</p>
-				<button className="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false" ref={navToggleRef}>
-					<span className="sr-only">Menu</span>
-				</button>
-				<nav className="nav-container">
-					<ul id="primary-navigation" data-visible="false" className="primary-navigation flex" ref={primaryNavRef}>
-						<li className="link">
-							<a href="/">Home</a>
-						</li>
-						<li className="link">
-							<a href="/products">Products</a>
-						</li>
-						<li className="link">
-							<a href="/custom">Customize</a>
-						</li>
-						<li className="link">
-							<a href="/contacts">Contacts</a>
-						</li>
-					</ul>
-				</nav>
-				<div className="cart-cont">
-					<button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
-						<AiOutlineShopping />
-						<span className="cart-item-qty">{totalQuantities}</span>
-					</button>
-				</div>
-				{showCart && <Cart />}
-			</div>
-		</div>
-	);
+      console.log("Visibility:", visibility); // Debug statement
+    };
+
+    navToggle.addEventListener("click", handleClick);
+
+    return () => {
+      // Cleanup the event listener when the component unmounts
+      navToggle.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  return (
+    <div className="navbar-container">
+      <div className="navbar-inner-cont">
+        <p className="logo">
+          <Link href="/">Shopfeast</Link>
+        </p>
+        <button
+          className="mobile-nav-toggle"
+          aria-controls="primary-navigation"
+          aria-expanded="false"
+          ref={navToggleRef}
+        >
+          <span className="sr-only">Menu</span>
+        </button>
+        <nav className="nav-container">
+          <ul
+            id="primary-navigation"
+            data-visible="false"
+            className="primary-navigation flex"
+            ref={primaryNavRef}
+          >
+            <li className="link">
+              <a href="/">Home</a>
+            </li>
+            <li className="link">
+              <a href="/products">Products</a>
+            </li>
+            <li className="link">
+              <a href="/custom">Customize</a>
+            </li>
+            <li className="link">
+              <a href="/contacts">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="cart-cont">
+          <button
+            type="button"
+            className="cart-icon"
+            onClick={() => setShowCart(true)}
+          >
+            <AiOutlineShopping />
+            <span className="cart-item-qty">{totalQuantities}</span>
+          </button>
+        </div>
+        {showCart && <Cart />}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

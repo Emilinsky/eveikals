63ec9d8913545e5fc9a3d6122febe7bd51b1e03f
fansters <edmundseizentals@gmail.com>
2023-06-04 12:35:09 +0300

    marged footer, made some basic changes. Created contact page and modular.css

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,96 +4,87 @@ import { AiFillInstagram, AiOutlineFacebook } from "react-icons/ai";
 
 import styles from "../styles/Footer.module.css";
 
+function Footer() {
+	return (
+		<>
+			<footer className={styles.footer}>
+				<div className={styles.footer__addr}>
+					<h1 className={styles.footer__logo}>
+						<Link href='#'>Shopfeast</Link>
+					</h1>
 
+					<h2>Contact</h2>
 
-function App() {
-  return (
-    <div>
-      <header>
-        {/* Content */}
-      </header>
+					<address>
+						Edmund, from Shopfeast
+						<a className={styles.footer__btn} href='mailto:edmund@shopfeast.com'>
+							Email Us
+						</a>
+					</address>
+				</div>
 
-      <main>
-        {/* Content */}
-      </main>
+				<ul className={styles.footer__nav}>
+					<li className={styles.nav__item}>
+						<h2 className={styles.nav__title}>Media</h2>
 
-      <footer className={styles.footer}>
-        <div className={styles.footer__addr}>
-          <h1 className={styles.footer__logo}>
-			<Link href="#">Shopfeast</Link>
-			</h1>
+						<ul className={styles.nav__ul}>
+							<li>
+								<a href='#'>Online</a>
+							</li>
 
-          <h2>Contact</h2>
+							<li>
+								<a href='#'>Print</a>
+							</li>
 
-          <address>
-            Edmund, from Shopfeast
-            <a className={styles.footer__btn} href="mailto:edmund@shopfeast.com">Email Us</a>
-          </address>
-        </div>
+							<li>
+								<a href='#'>Alternative Ads</a>
+							</li>
+						</ul>
+					</li>
 
-        <ul className={styles.footer__nav}>
-          <li className={styles.nav__item}>
-            <h2 className={styles.nav__title}>Media</h2>
+					<li className={(styles.nav__item, styles.nav__item_extra)}>
+						<h2 className={styles.nav__title}>Technology</h2>
 
-            <ul className={styles.nav__ul}>
-              <li>
-                <a href="#">Online</a>
-              </li>
+						<ul className={styles.nav__ul}>
+							<li>
+								<a href='#'>Hardware Design</a>
+							</li>
 
-              <li>
-                <a href="#">Print</a>
-              </li>
+							<li>
+								<a href='#'>Software Design</a>
+							</li>
 
-              <li>
-                <a href="#">Alternative Ads</a>
-              </li>
-            </ul>
-          </li>
+							<li>
+								<a href='#'>Digital Signage</a>
+							</li>
+						</ul>
+					</li>
 
-          <li className={styles.nav__item, styles.nav__item_extra}>
-            <h2 className={styles.nav__title}>Technology</h2>
+					<li className={styles.nav__item}>
+						<h2 className={styles.nav__title}>Legal</h2>
 
-            <ul className={styles.nav__ul}>
-              <li>
-                <a href="#">Hardware Design</a>
-              </li>
+						<ul className={styles.nav__ul}>
+							<li>
+								<a href='#'>Privacy Policy</a>
+							</li>
 
-              <li>
-                <a href="#">Software Design</a>
-              </li>
+							<li>
+								<a href='#'>Terms of Use</a>
+							</li>
 
-              <li>
-                <a href="#">Digital Signage</a>
-              </li>
+							<li>
+								<a href='#'>Sitemap</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
 
-            </ul>
-          </li>
-
-          <li className={styles.nav__item}>
-            <h2 className={styles.nav__title}>Legal</h2>
-
-            <ul className={styles.nav__ul}>
-              <li>
-                <a href="#">Privacy Policy</a>
-              </li>
-
-              <li>
-                <a href="#">Terms of Use</a>
-              </li>
-
-              <li>
-                <a href="#">Sitemap</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-
-        <div className={styles.legal}>
-          <p>&copy; 2023. Shopfeast. All rights reserved.</p>
-        </div>
-      </footer>
-    </div>
-  );
+				<div className={styles.legal}>
+					<p>&copy; 2023. Shopfeast. All rights reserved.</p>
+				</div>
+			</footer>
+		</>
+	);
 }
 
-export default App;
+export default Footer;
---
+++ pages/contacts.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Contacts.module.css";
+
+const contacts = () => {
+	return <div className={styles.contact_cont}>contact</div>;
+};
+
+export default contacts;

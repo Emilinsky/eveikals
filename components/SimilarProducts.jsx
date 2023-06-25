60a66dde82044742fa560c0d99ddc7c0cff74ff2
fansters <edmundseizentals@gmail.com>
2023-06-25 23:09:19 +0300

    added responsivness to similar products, and changed how many products are displayed depending on width size. 95% finished. Need to add anims from framer motion

--- components/SimilarProducts.jsx
+++ components/SimilarProducts.jsx
@@ -13,25 +13,57 @@ function SimilarProducts({ products, currentTag, currentProductId }) {
 		return product.tags.some((tag) => currentTag.includes(tag));
 	});
 
-	// resetting carousel on every product selected
+	// State to keep track of the index of the first product currently being displayed
+	const [startIndex, setStartIndex] = useState(0);
+
+	// State to keep track of the number of products to display at a time
+	const [displayCount, setDisplayCount] = useState(5);
+
+	// Listen for window resize events to update displayCount
+	// Listen for window resize events to update displayCount
+	useEffect(() => {
+		// Check if component is mounted (in browser)
+		if (typeof window !== "undefined") {
+			const handleResize = () => {
+				const width = window.innerWidth;
+				if (width < 600) {
+					setDisplayCount(2);
+				} else if (width < 1000) {
+					setDisplayCount(4);
+				} else if (width < 1300) {
+					setDisplayCount(3);
+				} else if (width < 1550) {
+					setDisplayCount(4);
+				} else {
+					setDisplayCount(5);
+				}
+			};
+
+			// Initial setting based on window width
+			handleResize();
+
+			window.addEventListener("resize", handleResize);
+
+			// Clean up the event listener when the component unmounts
+			return () => {
+				window.removeEventListener("resize", handleResize);
+			};
+		}
+	}, []);
+
+	// Resetting carousel on every product selected
 	useEffect(() => {
 		setStartIndex(0);
 	}, [currentTag]);
 
-	// State to keep track of the index of the first product currently being displayed
-	const [startIndex, setStartIndex] = useState(0);
-
-	// Number of products to display at a time
-	const displayCount = 5;
-
 	// Function to handle the "Next" button click
 	const handleNextClick = () => {
-		setStartIndex((prevIndex) => Math.min(prevIndex + displayCount, filteredProducts.length - displayCount));
+		setStartIndex((prevIndex) => Math.min(prevIndex + 1, filteredProducts.length - displayCount));
 	};
 
 	// Function to handle the "Previous" button click
 	const handlePrevClick = () => {
-		setStartIndex((prevIndex) => Math.max(prevIndex - displayCount, 0));
+		setStartIndex((prevIndex) => Math.max(prevIndex - 1, 0));
 	};
 
 	// Slice the filteredProducts array to only include the products that should be currently visible

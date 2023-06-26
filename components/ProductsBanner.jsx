2b922cc6586ac93e7f871f101bd421681df4d4b5
fansters <edmundseizentals@gmail.com>
2023-06-26 20:21:51 +0300

    fixing and finalazing porductBanner.

--- components/ProductsBanner.jsx
+++ components/ProductsBanner.jsx
@@ -20,7 +20,7 @@ const ProductsBanner = ({ ProductsBanner, colors, sizes }) => {
 			<div className={`${styles.container} ${isRightPanelActive ? styles.right_panel_active : ""}`}>
 				{/* NEWST PRODUCT ADDITION */}
 				<div className={`${styles.form_container} ${styles.sign_up_container}`}>
-					<div className={styles.div_container}>
+					<div className={styles.div_container_new}>
 						<h1 className={styles.product_heading}>{ProductsBanner.headingRight}</h1>
 						<img src={urlFor(ProductsBanner.imageNew)} alt='shirts' className={styles.products_banner_img} />
 					</div>
@@ -28,41 +28,41 @@ const ProductsBanner = ({ ProductsBanner, colors, sizes }) => {
 
 				{/* PRODUCT OF THE WEEK */}
 				<div className={`${styles.form_container} ${styles.sign_in_container}`}>
-					<div className={styles.colors_top}>
-						<div className={styles.colors_cont}>
-							<h2 className={styles.colors_heading}>
-								Available
-								<br />
-								colors:
-							</h2>
-							{colors.map((color) => (
-								<div className={styles.colors_content} key={color.colorName}>
-									<p className={styles.color_code} style={{ backgroundColor: color.colorCode }}>
-										<span>{color.colorName}</span>
-									</p>
-								</div>
-							))}
-						</div>
-					</div>
-               <div className={styles.sizes_top}>
-						<div className={styles.sizes_cont}>
-							<h2 className={styles.sizes_heading}>
-								Available
-								<br />
-								sizes:
-							</h2>
-							{sizes.map((size) => (
-								<div className={styles.sizes_content} key={size.size}>
-									<p className={styles.size_name}>
-										<span>{size.size}</span>
-									</p>
-								</div>
-							))}
-						</div>
-					</div>
-					<div className={styles.div_container}>
+					<div className={styles.div_container_top}>
 						<h1 className={styles.product_heading}>{ProductsBanner.headingLeft}</h1>
 						<img src={urlFor(ProductsBanner.imageTop)} alt='shirts' className={styles.products_banner_img} />
+						<div className={styles.options_cont}>
+							{/* colors opt */}
+							<div className={styles.colors_top}>
+								<div className={styles.colors_cont}>
+									<h2 className={styles.colors_heading}>Available colors:</h2>
+									<div className={styles.colors_outter_cont}>
+										{colors.map((color) => (
+											<div className={styles.colors_content} key={color.colorName}>
+												<p className={styles.color_code} style={{ backgroundColor: color.colorCode }}>
+													{/* <span>{color.colorName}</span> */}
+												</p>
+											</div>
+										))}
+									</div>
+								</div>
+							</div>
+							{/* size opt */}
+							<div className={styles.sizes_top}>
+								<div className={styles.sizes_cont}>
+									<h2 className={styles.sizes_heading}>Available sizes:</h2>
+									<div className={styles.sizes_outter_cont}>
+										{sizes.map((size) => (
+											<div className={styles.sizes_content} key={size.size}>
+												<p className={styles.size_name}>
+													<span>{size.size}</span>
+												</p>
+											</div>
+										))}
+									</div>
+								</div>
+							</div>
+						</div>
 						<p className={styles.sales_num}>{`Sales last week: ${ProductsBanner.salesCount}`}</p>
 					</div>
 				</div>

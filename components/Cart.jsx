63bfe7c8d21bf9d2ebede7d7629353247fd11dd4
fansters <edmundseizentals@gmail.com>
2023-06-27 16:49:56 +0300

    moved cart styles from global to cart.module. moving on to redesign the cart now.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -7,6 +7,7 @@ import toast from "react-hot-toast";
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
 import getStripe from "../lib/getStripe";
+import styles from "../styles/Cart.module.css";
 
 const Cart = () => {
 	const cartRef = useRef();
@@ -32,59 +33,59 @@ const Cart = () => {
 	};
 
 	return (
-		<div className='cart-wrapper' ref={cartRef}>
-			<div className='cart-container'>
-				<button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
+		<div className={styles.cart_wrapper} ref={cartRef}>
+			<div className={styles.cart_container}>
+				<button type='button' className={styles.cart_heading} onClick={() => setShowCart(false)}>
 					<AiOutlineLeft />
-					<span className='heading'>Your Cart</span>
-					<span className='cart-num-items'>{totalQuantities} items</span>
+					<span className={styles.heading}>Your Cart</span>
+					<span className={styles.cart_num_items}>{totalQuantities} items</span>
 				</button>
 				{cartItems.length < 1 && (
-					<div className='empty-cart'>
+					<div className={styles.empty_cart}>
 						<AiOutlineShopping size={150} />
 						<h3>Your cart is empty</h3>
 						<Link href='/'>
-							<button type='button' onClick={() => setShowCart(false)} className='btn'>
+							<button type='button' onClick={() => setShowCart(false)} className={styles.btn}>
 								Continue shopping
 							</button>
 						</Link>
 					</div>
 				)}
 
-				<div className='product-container'>
+				<div className={styles.product_container}>
 					{cartItems.length >= 1 &&
 						cartItems.map((item, i) => (
-							<div className='product' key={item.variant.id}>
+							<div className={styles.product} key={item.variant.id}>
 								{/* <img
 									src={urlFor(item.variantImage ? item.variantImage.src : item.image[0])}
 									className='cart-product-image'
 								/> */}
 								<img
 									src={item.variantImage ? item.variantImage.src : urlFor(item.image[0]).url()}
-									className='cart-product-image'
+									className={styles.cart_product_image}
 								/>
 
-								<div className='item-desc'>
-									<div className='flex top'>
+								<div className={styles.item_desc}>
+									<div className={`${styles.flex} ${styles.top}`}>
 										<h5>
 											{item.name}
 											{item.variant ? ` - ${item.variant.title}` : ""}
 										</h5>
 										<h4>€{item.variant ? item.variant.price : item.price}</h4>
 									</div>
-									<div className='flex bottom'>
+									<div className={`${styles.flex} ${styles.bottom}`}>
 										<div>
-											<p className='quantity-desc'>
-												<span className='minus' onClick={() => toggleCartItemQuantity(item.variant.id, "dec")}>
+											<p className={styles.quantity_desc}>
+												<span className={styles.minus} onClick={() => toggleCartItemQuantity(item.variant.id, "dec")}>
 													<AiOutlineMinus />
 												</span>
-												<span className='num'>{item.quantity}</span>
-												<span className='plus' onClick={() => toggleCartItemQuantity(item.variant.id, "inc")}>
+												<span className={styles.num}>{item.quantity}</span>
+												<span className={styles.plus} onClick={() => toggleCartItemQuantity(item.variant.id, "inc")}>
 													<AiOutlinePlus />
 												</span>
 											</p>
 										</div>
-										<button type='button' className='remove-item' onClick={() => onRemove(item)}>
+										<button type='button' className={styles.remove_item} onClick={() => onRemove(item)}>
 											<TiDeleteOutline />
 										</button>
 									</div>
@@ -93,13 +94,13 @@ const Cart = () => {
 						))}
 				</div>
 				{cartItems.length >= 1 && (
-					<div className='heart-bottom'>
-						<div className='total'>
+					<div className={styles.heart_bottom}>
+						<div className={styles.total}>
 							<h3>Total:</h3>
 							<h3>€{totalPrice.toFixed(2)}</h3>
 						</div>
-						<div className='btn-container'>
-							<button type='button' className='btn' onClick={handleCheckout}>
+						<div className={styles.btn_container}>
+							<button type='button' className={styles.btn} onClick={handleCheckout}>
 								Buy
 							</button>
 						</div>

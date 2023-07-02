17578d094d49ee95aa3e4395ec1d0dd026dd0656
fansters <edmundseizentals@gmail.com>
2023-07-02 17:16:50 +0300

    added google font in _document as its correct way of importing styles

--- pages/_document.js
+++ pages/_document.js
@@ -8,33 +8,37 @@ class MyDocument extends Document {
 		};
 	}
 
-	// 	render() {
-	// 		const csp = `
-	//     default-src 'self';
-	//   script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://js.stripe.com;
-	//   script-src-elem 'self' 'unsafe-inline' https://js.stripe.com;
-	//   worker-src 'self' blob:;
-	//   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-	//   img-src 'self' data: https: http:;
-	//   font-src 'self' data: https://fonts.gstatic.com 'https://fonts.googleapis.com';
-	//   connect-src 'self' https: http:;
-	//   object-src 'none';
-	//   frame-src https://m.youtube.com/ https://js.stripe.com/;
-	//   media-src 'self';
-	// `;
+	render() {
+		// 		const csp = `
+		//     default-src 'self';
+		//   script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: https://js.stripe.com;
+		//   script-src-elem 'self' 'unsafe-inline' https://js.stripe.com;
+		//   worker-src 'self' blob:;
+		//   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+		//   img-src 'self' data: https: http:;
+		//   font-src 'self' data: https://fonts.gstatic.com 'https://fonts.googleapis.com';
+		//   connect-src 'self' https: http:;
+		//   object-src 'none';
+		//   frame-src https://m.youtube.com/ https://js.stripe.com/;
+		//   media-src 'self';
+		// `;
 
-	// 		return (
-	// 			<Html>
-	// 				<Head>
-	// 					<meta httpEquiv='Content-Security-Policy' content={csp} />{" "}
-	// 				</Head>{" "}
-	// 				<body>
-	// 					<Main />
-	// 					<NextScript />
-	// 				</body>{" "}
-	// 			</Html>
-	// 		);
-	// 	}
+		return (
+			<Html>
+				<Head>
+					{/* <meta httpEquiv='Content-Security-Policy' content={csp} /> */}
+					<link
+						href='https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@600&display=swap'
+						rel='stylesheet'
+					/>{" "}
+				</Head>{" "}
+				<body>
+					<Main />
+					<NextScript />
+				</body>{" "}
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
